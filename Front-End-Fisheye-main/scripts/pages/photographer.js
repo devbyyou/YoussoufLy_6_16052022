8bbaab7aa30f57f216d9719006fb32a572e9036b
Aurélien Cabirol <aurelien.cabirol@webnation.tech>
2022-06-26 17:51:39 +0200

    Résolution bug

--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -1,29 +1,16 @@
-//Mettre le code JavaScript lié à la page photographer.html
-const getPhotographer = async function() {
-  // // et bien retourner le tableau photographers seulement une fois
+const getPhotographers2 = async function() {
   const photographers = await fetch("Front-End-Fisheye-main/data/photographers.json")
     .then((data) => data.json())
     .then((data) => data.photographers);
 
-  console.log(photographers);
-
   return photographers;
 }
-getPhotographer();
 
-
-const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
-
-const urlSearchParams = new URLSearchParams(queryString_url_id);
-console.log(urlSearchParams);
-
-const id = urlSearchParams.get("id");
-console.log(id);
-
-// d'abord aller chercher json pour pouvoir afficher ces oeuvre
-// pour aller chercher l'obj
-
-const idProduitSelectionner = getPhotographer.find((element) => element.id === id);
-
-console.log(idProduitSelectionner);
+getPhotographers2().then(
+    (photographers) => {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const id = urlSearchParams.get("id");
+      const photographer = photographers.find((p) => p.id == id);
+      console.log(photographer);
+    }
+);
