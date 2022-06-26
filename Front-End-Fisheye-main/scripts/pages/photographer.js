2258d792a9335581bca34360656b8648a9700e4b
Aurélien Cabirol <aurelien.cabirol@webnation.tech>
2022-06-26 19:11:51 +0200

    Création de la page photographer

--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -6,11 +6,15 @@ const getPhotographers2 = async function() {
   return photographers;
 }
 
+const main = document.querySelector('.photograph-header');
 getPhotographers2().then(
     (photographers) => {
       const urlSearchParams = new URLSearchParams(window.location.search);
       const id = urlSearchParams.get("id");
-      const photographer = photographers.find((p) => p.id == id);
-      console.log(photographer);
+      const photographer = photographers.find((p) => p.id === parseInt(id));
+      // Tu peux maintenant construire ta page avec les info du photographer
+      const title = document.createElement('h2');
+      title.innerText = photographer.name;
+      main.appendChild(title);
     }
 );
